1f53893c25ae646ed3660d779d8cf12a432aacbe
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-08-06 21:07:31 -0400

    input voltimeter

--- pituApp02/src/ofxInput.cpp
+++ pituApp02/src/ofxInput.cpp
@@ -6,4 +6,95 @@
 //  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
 //
 
-#include <iostream>
+#include "ofxInput.h"
+
+ofxInput::ofxInput(){
+    x = 0;
+    y = 0;
+    width = 100;
+    height = 10;
+    
+    minValue = 0.0;
+    maxValue = 1.0;
+    
+    color.set(255,0,0);
+    
+    vertOriented = false;
+};
+
+void ofxInput::setValue(float _value){
+    value = ofMap(_value, minValue, maxValue, 0.0, 1.0, true);
+};
+
+void ofxInput::update(){
+    if ( ofGetMousePressed() ){
+        float mouseX = ofGetMouseX();
+        float mouseY = ofGetMouseY();
+        
+        if ((mouseX >= x ) && 
+            (mouseX <= x + width) &&
+            (mouseY >= y ) && 
+            (mouseY <= y + height)){
+            
+            if ( !vertOriented ){
+                threshold = (mouseX - x) / width;
+            } else {
+                threshold = (mouseY - y) / height;
+            }
+        }
+    }
+    
+    color.setHue(value*100);
+};
+
+void ofxInput::draw(){
+    // draw the average volume:
+	ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(x, y, 0);
+    
+    
+    if ( !vertOriented ){
+        
+        //  Box bar
+        //
+        ofNoFill();		
+        ofSetColor(255,100);
+        ofRect(0, 0, width, height);
+        
+        //  Value bar
+        //
+        float padding = height*0.2;
+        ofFill();
+        ofSetColor(color,200);
+        ofRect( padding, padding, min(value*width, width-padding*2) , height-padding*2);
+    
+        //  Threshold Line
+        //
+        ofSetColor(255,150);
+        ofLine(threshold*width, 0, 
+               threshold*width, height);
+    } else {
+        //  Box bar
+        //
+        ofNoFill();		
+        ofSetColor(255,100);
+        ofRect(0, 0, width, height);
+        
+        //  Value bar
+        //
+        float padding = width*0.2;
+        ofFill();
+        ofSetColor(color,200);
+        ofRect( padding,height - padding - min( value*height + padding, height + padding) , width-padding*2 , min(value*height,height-padding*2) );
+        
+        //  Threshold Line
+        //
+        ofSetColor(255,150);
+        ofLine(0, threshold*height, 
+               width, threshold*height);
+    }
+    
+    ofPopMatrix();
+	ofPopStyle();
+};
--- pituApp02/src/ofxInput.h
+++ pituApp02/src/ofxInput.h
@@ -6,9 +6,37 @@
 //  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
 //
 
-#ifndef pituApp02_ofxInputSensorBar_h
-#define pituApp02_ofxInputSensorBar_h
+#ifndef OFXINPUTSENSORBAR
+#define OFXINPUTSENSORBAR
 
+#include "ofMain.h"
 
+class ofxInput : public ofRectangle {
+public:
+    
+    ofxInput();
 
+    void    setMin(float _minValue){minValue = _minValue;};
+    void    setMax(float _maxValue){maxValue = _maxValue;};
+    
+    void    setVerticalOriented( bool _vert = true){ vertOriented = _vert;};
+    void    setThreshold(float _threshold){threshold = _threshold;};
+    float   getThreshold(){return threshold;};
+    
+    void    setValue(float _value);
+    float   getValue(){return value;};
+    
+    void    update();
+    void    draw();
+    
+private:
+    
+    ofColor color;
+    float   maxValue;
+    float   minValue;
+    float   threshold;
+    float   value;
+    
+    bool    vertOriented;
+};
 #endif
